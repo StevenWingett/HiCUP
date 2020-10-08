6cab3c29ae06a477700ab5564b528fdcd822cedc
StevenWingett <wingetts@headstone.babraham.ac.uk>
2020-10-08 16:35:13 +0100

    Updated documentation for next release

--- hicup_module.pm
+++ hicup_module.pm
@@ -8,7 +8,7 @@ our @EXPORT = qw(VERSION hasval deduplicate_array checkR process_config check_fi
 our @EXPORT_OK = qw(hashVal outdirFileNamer check_no_duplicate_filename check_filenames_ok 
     checkAligner checkAlignerIndices newopen quality_checker determineAlignerFormat get_csome_position);
 
-our $VERSION = "0.7.5.dev";
+our $VERSION = "0.8.0";
 
 use Data::Dumper;
 use strict;
